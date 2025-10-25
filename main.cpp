e1d837c04ad7a0dc6309f578b084089743853b5e
Hvostov Daniil <daniil2007xboctob@gmail.com>
2025-10-25 17:53:01 +0300

    identified the necessary functions

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 #include <iostream>
+#include <limits>
+
+int get_max_int();
+bool is_mult_overflow(int a, int b, int c);
+bool is_sum_overflow(int a, int b);
+bool is_every_number_natural(int a, int b, int c);
+int get_max(int a, int b, int c);
+int get_first(int a, int b, int c);
+int get_second(int a, int b, int c);
+bool is_pythagoric(int a, int b, int c);
 
 int main()
 {
